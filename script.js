53193d01fd5c4d881db02dc78dcb4838f1839619
Conanas <tmhaigh@gmail.com>
2020-08-28 13:14:28 +1000

    add welcome screen object

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var questions = [{
     question1: "Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -8,7 +9,8 @@ var questions = [{
     answer: "Option 2"
 }, {
     question1: "Question 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -16,7 +18,8 @@ var questions = [{
     answer: "Option 3"
 }, {
     question1: "Question 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -24,7 +27,8 @@ var questions = [{
     answer: "Option 1"
 }, {
     question1: "Question 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -32,7 +36,8 @@ var questions = [{
     answer: "Option 2"
 }, {
     question1: "Question 5: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -40,7 +45,8 @@ var questions = [{
     answer: "Option 4"
 }, {
     question1: "Question 6: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
@@ -48,10 +54,17 @@ var questions = [{
     answer: "Option 2"
 }, {
     question1: "Question 7: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-    options: ["Option 1",
+    options: [
+        "Option 1",
         "Option 2",
         "Option 3",
         "Option 4"
     ],
     answer: "Option 1"
 }]
+
+var start = {
+    startTitle: "Coding Quiz Challenge",
+    startMessage: "Try to answer the following questions about coding within the time limit. Keep in mind that all incorrect answers will reduce the time left on the clock",
+    startButton: "Start"
+}
