6f7631f4bcfc15543a50fb78981bb9595bec5e3c
Conanas <tmhaigh@gmail.com>
2020-08-28 13:05:57 +1000

    added array of object questions

---
+++ script.js
@@ -0,0 +1,57 @@
+var questions = [{
+    question1: "Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 2"
+}, {
+    question1: "Question 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 3"
+}, {
+    question1: "Question 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 1"
+}, {
+    question1: "Question 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 2"
+}, {
+    question1: "Question 5: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 4"
+}, {
+    question1: "Question 6: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 2"
+}, {
+    question1: "Question 7: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+    options: ["Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4"
+    ],
+    answer: "Option 1"
+}]
